e6c3defad01f96ffafe1c16b6b418c96157b942c
Sam Good <144557038+NoMansion@users.noreply.github.com>
2025-04-23 19:13:54 -0500

    Update todo_list.rs

--- todo_list.rs
+++ todo_list.rs
@@ -1,38 +1,47 @@
-use std::collections::LinkedList;
-use crate::task::Task;
+// To-do list struct
+// Created by Sam Good
 
+use crate::task::Task;
+use std::collections::LinkedList;
+
+// struct definition
 pub struct ToDoList {
-    pub day: String,
+    pub title: String,
     pub is_complete: bool,
     pub list: LinkedList<Task>,
 }
 
 impl ToDoList {
-    pub fn new(day: &str) -> Self {
+    // constructor, takes a name for the to-do list
+    pub fn new(title: &str) -> Self {
         ToDoList {
-            day: day.to_string(),
+            title: title.to_string(),
             is_complete: false,
             list: LinkedList::new(),
         }
     }
 
+    // adds a task to the to-do list
     pub fn add_task(&mut self, task: Task) {
         self.list.push_back(task);
     }
 
+    // removes a task from the to-do list
+    // takes the name of the task
     pub fn remove_task(&mut self, name: &str) {
         let old_list = std::mem::take(&mut self.list);
         let mut new_list = LinkedList::new();
-    
+
         for task in old_list {
             if task.get_name() != name {
                 new_list.push_back(task);
             }
         }
-    
-        self.list = new_list;
-    }    
 
+        self.list = new_list;
+    }
+
+    // marks all tasks as complete
     pub fn mark_all_complete(&mut self) {
         for task in &mut self.list {
             task.mark_complete();
@@ -40,17 +49,24 @@ impl ToDoList {
         self.is_complete = true;
     }
 
-    pub fn mark_all_uncomplete(&mut self) {
+    // marks all tasks as incomplete
+    pub fn mark_all_incomplete(&mut self) {
         for task in &mut self.list {
-            task.mark_uncomplete();
+            task.mark_incomplete();
         }
         self.is_complete = false;
     }
 
+    // prints the title and all tasks with their desciptions
     pub fn print(&self) {
-        println!("To-do list {}:", self.day);
+        println!("To-do list {}:", self.title);
         for task in &self.list {
             task.print();
         }
+    }
+
+    // checks if a to-do list is empty
+    pub fn check_empty(&self) -> bool {
+        self.list.is_empty()
     }
 }
